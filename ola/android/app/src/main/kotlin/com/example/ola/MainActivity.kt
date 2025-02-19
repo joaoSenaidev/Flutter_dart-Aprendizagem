1711c73c1ca00532017b9d7ecaf6daba80347b59
Joao Pedro <joaocruz.oliver17@gmail.com>
2025-02-18 22:30:19 -0300

    Aprendendo Flutter

---
+++ ola/android/app/src/main/kotlin/com/example/ola/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ola
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ola/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'app.widget.dart';
+
+main() {
+  runApp(Appwidget());
+}
